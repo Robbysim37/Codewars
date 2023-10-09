1068c9fabfa93b07831ad5ca3b258945956da9db
Robby Lewis <Robbysim37@gmail.com>
2023-10-09 14:41:33 -0400

    Java

--- Java/sumOfDigits/src/sumOfDigitsCalc.java
+++ Java/sumOfDigits/src/sumOfDigitsCalc.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class sumOfDigitsCalc {
+public class sumOfDigitsCalc {
+    public static char[] calc(int arg) {
+        String numberString = String.valueOf(arg);
+        char[] digitArr = numberString.toCharArray();
+
+        return digitArr;
+    }
 }
